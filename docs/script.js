e23a22b73ff15a918a534e755b71f62a45c9882d
KekSy <116192633+KekSy123@users.noreply.github.com>
2026-04-08 14:07:17 +0200

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,5 @@
+function showSection(sectionId) 
+{
+    document.querySelectorAll('.section').forEach(sec => sec.classList.remove('active'));
+    document.getElementById(sectionId).classList.add('active');
+}
